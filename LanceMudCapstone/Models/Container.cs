f371ba31b385259b570486291ff55df98d85ed2a
Tagwaggle <164380777+Tagwaggle@users.noreply.github.com>
2026-01-26 11:42:22 -0600

    Adding second repo for deployment

--- LanceMudCapstone/Models/Container.cs
+++ LanceMudCapstone/Models/Container.cs
@@ -1,6 +1,13 @@
-﻿namespace LanceMudCapstone.Models
+﻿namespace LanceMudCapstone.Models;
+
+public class Container
 {
-    public class Container
-    {
-    }
+    public int ContainerId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int RoomId { get; set; }
+    public int? OwnerNPCId { get; set; }
+    public bool IsLootable { get; set; }
+    public DateTime CreatedAt { get; set; }
+
 }
+ 
