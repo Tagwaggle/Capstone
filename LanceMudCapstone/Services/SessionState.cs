837acaea38cbd24fd41d78d9140dd6c0d19fecd7
Tagwaggle <164380777+Tagwaggle@users.noreply.github.com>
2026-02-08 11:04:46 -0600

    cleaned up layouts for better hydration

--- LanceMudCapstone/Services/SessionState.cs
+++ LanceMudCapstone/Services/SessionState.cs
@@ -1,11 +1,13 @@
 ﻿using LanceMudCapstone.DTOs;
 using LanceMudCapstone.Models;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LanceMudCapstone.Services;
+
 
 public class SessionState
 {
